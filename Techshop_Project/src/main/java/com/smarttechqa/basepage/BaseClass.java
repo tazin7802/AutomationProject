07a1a8de0249ce66d9937b4f25a21d83f13930e8
tazin <tazin@TazinsPc.attlocal.net>
2023-05-05 21:49:21 -0500

    love update

--- Techshop_Project/src/main/java/com/smarttechqa/basepage/BaseClass.java
+++ Techshop_Project/src/main/java/com/smarttechqa/basepage/BaseClass.java
@@ -70,7 +70,7 @@ public class BaseClass {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
 		BaseClass.setUp();// This is how we call Static class
-		
+		System.out.println(" I love you");
 		
 	}
 
